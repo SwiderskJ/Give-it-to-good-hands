186b5c0089174b67fbbc32967ebbab7f052fbb6e
SwiderskJ <janswiderski@op.pl>
2023-03-15 15:43:47 +0100

    Update views.py

--- donation_app/views.py
+++ donation_app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from donation_app.models import Donation, Institution
 
 
 class LandingPageView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        num_of_donations = 0
+        donations = Donation.objects.all()
+        for donation in donations:
+            donation.quantity += num_of_donations
+        num_of_institutions = Institution.objects.all().count()
+        if num_of_institutions is None:
+            num_of_institutions = 0
+        return render(request, 'index.html', context={
+            'donations': num_of_donations,
+            'institutions': num_of_institutions,
+        })
 
 
 class AddDonationView(View):
