5430bc1d158af4125f13aeb1842dd4c3ca1cc844
SwiderskJ <janswiderski@op.pl>
2023-03-15 15:06:02 +0100

    Update models.py
    Add model Donation

--- donation_app/models.py
+++ donation_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 FUNDATION_TYPE = (
     (1, 'Fundacja'),
@@ -16,3 +17,17 @@ class Institution(models.Model):
     description = models.CharField(max_length=256)
     type = models.IntegerField(choices=FUNDATION_TYPE, default=1)
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=16)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, default = None)
