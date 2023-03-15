276b41308c0331de2d98851d7f6cf313853eea77
SwiderskJ <janswiderski@op.pl>
2023-03-15 13:50:41 +0100

    Update views.py

--- donation_app/views.py
+++ donation_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class AddDonationView(View):
+
+    def get(self, request):
+        return render(request, 'form.html')
+
