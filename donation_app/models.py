694ea99a57e96bba9c9d004d0b4ace2823113e82
SwiderskJ <janswiderski@op.pl>
2023-03-15 14:44:55 +0100

    Update models.py
    Implement model Institution

--- donation_app/models.py
+++ donation_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+FUNDATION_TYPE = (
+    ('fundacja', 'Fundacja'),
+    ('op', 'Organizacja pozarządowa'),
+    ('zl', 'Zbiórka lokalna'),
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    type = models.CharField(choices=FUNDATION_TYPE, default='fundacja')
