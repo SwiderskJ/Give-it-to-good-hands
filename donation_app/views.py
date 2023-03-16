1c73b7fa7a588f847eaa6fc9f45c82c21682fb81
SwiderskJ <janswiderski@op.pl>
2023-03-16 13:21:22 +0100

    Update views.py

--- donation_app/views.py
+++ donation_app/views.py
@@ -12,25 +12,28 @@ class LandingPageView(View):
         donations = Donation.objects.all()
 
         for donation in donations:
-            donation.quantity += num_of_donations
+            num_of_donations += donation.quantity
         num_of_institutions = Institution.objects.all().count()
-
         if num_of_institutions is None:
             num_of_institutions = 0
 
-        fundations = []
-        fundations_list = Institution.objects.filter(type=1)
-        for item in fundations_list:
-    
-        print(fundations)
-        page_number = request.GET.get('page')
-
+        page = request.GET.get('page')
+        foundations = Institution.objects.filter(type=1).order_by('name')
+        paginator_foundations = Paginator(foundations, 5)
+        foundations = paginator_foundations.get_page(page)
+        organizations = Institution.objects.filter(type=2).order_by('name')
+        paginator_organizations = Paginator(organizations, 5)
+        organizations = paginator_organizations.get_page(page)
+        local_collection = Institution.objects.filter(type=3).order_by('name')
+        paginator_local_collections = Paginator(local_collection, 5)
+        local_collection = paginator_local_collections.get_page(page)
 
         return render(request, 'index.html', context={
             'donations': num_of_donations,
             'institutions': num_of_institutions,
-            'fundations': fundations,
-
+            "foundations": foundations,
+            "organizations": organizations,
+            "collections": local_collection,
         })
 
 
