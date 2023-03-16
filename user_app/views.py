74c3a65703660567da6e9ddf9107b7280407e1a0
SwiderskJ <janswiderski@op.pl>
2023-03-16 15:01:05 +0100

    Add registration and login

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+
+from user_app.forms import NewUserForm
 
 
 class LoginView(View):
@@ -7,9 +14,36 @@ class LoginView(View):
     def get(self, request):
         return render(request, 'login.html')
 
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            username=username,
+            password=password,
+        )
+
+        if user:
+            login(request, user)
+            return redirect(reverse('main_site'))
+        else:
+            messages.success(request, "Niepoprawne dane")
+            return redirect(reverse('register'))
+
 
 class RegisterView(View):
 
     def get(self, request):
-        return render(request, 'register.html')
+        form = NewUserForm()
+        return render(request, 'register.html', context={'form': form})
 
+    def post(self, request):
+
+        User.objects.create_user(
+            username=request.POST.get('username'),
+            password=request.POST.get('password1'),
+            last_name=request.POST.get('last_name'),
+            email=request.POST.get('email')
+            )
+        messages.success(request, "Registration successful.")
+        return redirect(reverse('login'))
